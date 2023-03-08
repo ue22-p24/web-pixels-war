340af45e57091675c81d955f090018c776896d9d
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2023-03-08 10:42:23 +0100

    clean up the skeleton: no global function

--- pixel.js
+++ pixel.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
             //TODO: attacher au clic de chaque pixel une fonction qui demande au serveur de colorer le pixel sous là forme :
             // http://pixels-war.oie-lab.net/set/id/x/y/r/g/b
-            // la fonction getPickedColorInRGB ci-dessous peut aider 
+            // la fonction getPickedColorInRGB ci-dessous peut aider
 
             //TODO: pourquoi pas rafraichir la grille toutes les 3 sec ?
 
@@ -20,28 +20,29 @@ document.addEventListener("DOMContentLoaded", () => {
             //TODO: pour les avancés: ça pourrait être utile de pouvoir
             // choisir la couleur à partir d'un pixel ?
         })
+
+    //TODO: pour les personnes avancées, comment transformer les deux "then" ci-dessus en "asynch / await" ?
+
+    //A compléter puis à attacher au bouton refresh en passant mon id une fois récupéré
+    function refresh(id) {
+        fetch(PIXEL_URL+"getmap?id="+id)
+            .then((response) => response.json())
+            .then((json) => {
+                //TODO: maintenant que j'ai le json des deltas, mettre à jour les pixels qui ont changé.
+                //"Here be dragons" : comment récupérer le bon div ?
+
+            })
+    }
+
+    //Petite fonction facilitatrice pour récupérer la couleur cliquée en RGB
+    function getPickedColorInRGB() {
+        let colorHexa = document.getElementById("colorpicker").value
+
+        let r = parseInt(colorHexa.substring(1,3),16);
+        let g = parseInt(colorHexa.substring(3,5),16);
+        let b = parseInt(colorHexa.substring(5,7),16);
+
+        return [r, g, b];
+    }
+
 })
-
-//TODO: pour les personnes avancées, comment transformer les deux "then" ci-dessus en "asynch / await" ?
-
-//A compléter puis à attacher au bouton refresh en passant mon id une fois récupéré
-function refresh(id) {
-    fetch(PIXEL_URL+"getmap?id="+id)
-        .then((response) => response.json())
-        .then((json) => {
-            //TODO: maintenant que j'ai le json des deltas, mettre à jour les pixels qui ont changé.
-            //"Here be dragons" : comment récupérer le bon div ?
-            
-        })
-}
-
-//Petite fonction facilitatrice pour récupérer la couleur cliquée en RGB
-function getPickedColorInRGB() {
-    let colorHexa = document.getElementById("colorpicker").value
-
-    let r = parseInt(colorHexa.substring(1,3),16);
-    let g = parseInt(colorHexa.substring(3,5),16);
-    let b = parseInt(colorHexa.substring(5,7),16);
-
-    return [r, g, b];
-}
