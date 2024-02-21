72c1051513d11b16675b97396155c1e9d88a41d2
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2024-02-21 16:17:54 +0100

    another helper function to pick a color from a pixel

--- pixel.js
+++ pixel.js
@@ -51,4 +51,22 @@ document.addEventListener("DOMContentLoaded", () => {
         return [r, g, b]
     }
 
+    // dans l'autre sens, pour mettre la couleur d'un pixel dans le color picker
+    // (le color picker insiste pour avoir une couleur en hexadécimal...)
+    function pickColorFrom(div) {
+        // plutôt que de prendre div.style.backgroundColor
+        // dont on ne connait pas forcément le format
+        // on utilise ceci qui retourne un 'rbg(r, g, b)'
+        const bg = window.getComputedStyle(div).backgroundColor
+        // on garde les 3 nombres dans un tableau de chaines
+        const [r, g, b] = bg.match(/\d+/g)
+        // on les convertit en hexadécimal
+        const rh = parseInt(r).toString(16).padStart(2, '0')
+        const gh = parseInt(g).toString(16).padStart(2, '0')
+        const bh = parseInt(b).toString(16).padStart(2, '0')
+        const hex = `#${rh}${gh}${bh}`
+        // on met la couleur dans le color picker
+        document.getElementById("colorpicker").value = hex
+    }
+
 })
